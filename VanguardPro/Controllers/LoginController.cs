3801b729b39b62e6f900d80e5651453cedf22b97
Vincboo <vincentboo7733@gmail.com>
2023-06-05 02:22:53 +0800

    update logincontroller

--- VanguardPro/Controllers/LoginController.cs
+++ VanguardPro/Controllers/LoginController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VanguardPro.Models;
 
 namespace VanguardPro.Controllers
 {
     public class LoginController : Controller
     {
+        private db_vanguardproEntities db = new db_vanguardproEntities();
+
         // GET: Login
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(tb_user objk)
+        {
+            if (ModelState.IsValid)
+            {
+                using (db_vanguardproEntities db = new db_vanguardproEntities()) ;
+                {
+                    var obj = db.tb_user.Where(a => a.u_username.Equals(objk.u_username) && a.u_pwd.Equals(objk.u_pwd)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.u_id.ToString();
+                        Session["UserName"] = obj.u_username.ToString();
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The Username or Password Incorrect");
+
+
+                    }
+                }
+            }
+
             return View();
         }
     }
