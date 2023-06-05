28e9ecb1e71ccff48cbbca66caa445ae5a5bd368
yejui <97009588+edward626@users.noreply.github.com>
2023-06-05 21:17:36 +0800

    Update investor controller & database

---
+++ VanguardPro/Controllers/tb_userController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VanguardPro.Models;
+
+namespace VanguardPro.Controllers
+{
+    public class tb_userController : Controller
+    {
+        private db_vanguardproEntities db = new db_vanguardproEntities();
+
+        // GET: tb_user
+        public ActionResult Index()
+        {
+            return View(db.tb_user.ToList());
+        }
+
+        // GET: tb_user/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_user tb_user = db.tb_user.Find(id);
+            if (tb_user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_user);
+        }
+
+        // GET: tb_user/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tb_user/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "u_id,u_username,u_pwd,u_email,u_usertype")] tb_user tb_user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (tb_user.u_usertype == 2)
+                {
+                    db.tb_user.Add(tb_user);
+                    db.SaveChanges();
+                    return RedirectToAction("Create","tb_investors", new { i_uid = tb_user.u_id });
+                }
+                else
+                {
+                    db.tb_user.Add(tb_user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+            return View(tb_user);
+        }
+
+        // GET: tb_user/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_user tb_user = db.tb_user.Find(id);
+            if (tb_user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_user);
+        }
+
+        // POST: tb_user/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "u_id,u_username,u_pwd,u_email,u_usertype")] tb_user tb_user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tb_user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tb_user);
+        }
+
+        // GET: tb_user/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_user tb_user = db.tb_user.Find(id);
+            if (tb_user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_user);
+        }
+
+        // POST: tb_user/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tb_user tb_user = db.tb_user.Find(id);
+            db.tb_user.Remove(tb_user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
